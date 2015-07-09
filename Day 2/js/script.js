94f108c18a9fa6fe2a9e2c486b92dd68c0037be4
xpeng1991 <xpeng@fullsail.edu>
2015-07-09 19:18:15 -0400

    Created starting files for Day 2
    Created files in web storm

---
+++ Day 2/js/script.js
@@ -0,0 +1,20 @@
+/*
+This is going to be a multi lined comment
+Xiang Peng
+7-9-15
+DSI Section 00
+Day 2 Coding Basics
+ */
+
+//alert("This is making sure that our js file is connected");
+
+//This is a single lined comment
+//Usually will describe what comes after
+
+/*
+multi-lined comment
+anything between the stars and slashes is ignored by the browser
+ */
+
+//Alert - gives the user information but returns nothing to us
+alert("blunt");
