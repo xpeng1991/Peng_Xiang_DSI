cb08924fac92e38e6e6cb13e1932062abf39d006
xpeng1991 <xpeng@fullsail.edu>
2015-07-09 22:41:17 -0400

    finished some questions
    feel good !!!

---
+++ Day3LabExpressions/js/Day3.js
@@ -0,0 +1,60 @@
+/*
+ This is going to be a multi lined comment
+ Xiang Peng
+ 7-9-15
+ DSI Section 00
+ Day3LabExpressions
+ */
+
+
+
+
+// Slice of Pie part 1
+
+// declare and initiation variable;
+var slices = 8;
+var people = 16;
+var pizza = 4;
+var slicePerPerson;
+
+//solve the math problem
+slicePerPerson = pizza * slices / people;
+
+// show us the result
+console.log("Each person ate " + slicePerPerson + " slices of pizza at the party.");
+
+// Slice of Pie part 2
+
+// declare another variable for Sparky
+var sparky;
+
+// sovle the math problem
+sparky = pizza * slices % people;
+
+// show result
+console.log("Sparky got " + sparky + " slices of pizza.");
+
+
+// Average shopping bill
+
+// declare and initiation variables;
+var week1 = 32;
+var week2 = 43;
+var week3 = 123;
+var week4 = 89;
+var week5 = 67;
+
+// solve math problems
+var total = week1 + week2 + week3 + week4 + week5;
+var average = (week1 + week2 + week3 + week4 + week5) / 5;
+
+// show result
+console.log("You have spent a total of $" + total +
+    " on groceries over 5 weeks. That is an average of $" + average + " per week");
+
+
+// Dis
+
+
+
+
--- Day3LabExpressions/js/script.js
+++
@@ -1,8 +0,0 @@
-/*
- This is going to be a multi lined comment
- Xiang Peng
- 7-9-15
- DSI Section 00
- Day3LabExpressions
- */
-
