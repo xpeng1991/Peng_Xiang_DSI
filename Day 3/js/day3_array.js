c1619fb59dda850067de959b28ec5bebd493947f
xpeng1991 <xpeng@fullsail.edu>
2015-07-11 18:55:28 -0400

    pop push splice
    pop push splice

--- Day 3/js/day3_array.js
+++ Day 3/js/day3_array.js
@@ -8,6 +8,21 @@ Arrays
 
 //alert("alert");
 
+// create a basic array
+var arr = [50, 145, 10005];
+
+//length property
+console.log(arr.length);
+
+// push - add to the end of the array
+arr.push(45);
+
+// pop - remove the last item of an array
+// and returns it
+arr.pop();
 
 
+// splice - add or remove items in an array
+// splice(position, number of items to remove, items to put in);
 
+arr.splice(1, 1);
