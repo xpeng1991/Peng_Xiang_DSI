5f452c948753088faf138f29b220e7b0a5e6243e
xpeng1991 <xpeng@fullsail.edu>
2015-07-11 19:36:19 -0400

    casting
    casting

---
+++ Day 3/js/Day3_Casting.js
@@ -0,0 +1,9 @@
+/*
+ Xiang Peng
+ Section 00
+ 7-11-15
+ Casting variables
+ */
+
+
+alert("Testing to see if this works");
--- Day 3/js/day3_prompts.js
+++ Day 3/js/day3_prompts.js
@@ -6,4 +6,32 @@
  */
 
 
-alert("Testing to see if this works");
+//alert("Testing to see if this works");
+
+
+// ask user for information
+// input
+var userInput = prompt("Enter your year of birth:");
+
+console.log(userInput);
+
+var age = 2015 - userInput;
+console.log(age);
+
+
+// calculate the area of a rectangle
+
+// prompt the user for width
+var width = prompt("Let's calculate the area of a rectangle.\n" +
+    "Please enter a width");
+
+// prompt the user for height
+var height = prompt("Please enter a height");
+
+// calculate the area using the user input
+var area = width * height;
+console.log(area);
+
+// create a variable that holds the whole result
+var result = "The area of your rectangle is " + area;
+console.log(result);
