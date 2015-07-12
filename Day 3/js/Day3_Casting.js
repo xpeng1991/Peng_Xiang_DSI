9d119beb65f7eff81f537e0901f8f76afcfdc37e
xpeng1991 <xpeng@fullsail.edu>
2015-07-11 20:09:13 -0400

    casting
    parsing

--- Day 3/js/Day3_Casting.js
+++ Day 3/js/Day3_Casting.js
@@ -6,4 +6,56 @@
  */
 
 
-alert("Testing to see if this works");
+//alert("Testing to see if this works");
+
+// Casting variables means treating one variable like another
+var stringVar = "6";
+var results = 7 + stringVar;
+console.log(results);
+
+// Number() - treats what is inside of the parenthesis like a number if possible
+var castingResults = 7 + Number(stringVar);
+console.log(castingResults);
+
+// String() - cast numbers as string
+var areaCode = 487;
+var prefix = 555;
+var lineNumber = 1234;
+
+var phoneNumber = String(areaCode) + String(prefix) + String(lineNumber);
+console.log(phoneNumber);
+
+
+// all prompt() returns text strings
+var vGC = prompt("how many games do u have?");
+
+var gB = prompt("how many game did u buy today?");
+
+var tG =  Number(vGC) + Number(gB);
+console.log(tG);
+
+
+// ParseInt - look through the string and returns the first integer
+// The text string MUST be starts with a NUMBER
+// Space are not counted
+// Returns NaN if cannot convert it
+
+var a = parseInt("40 years old");
+console.log(a);
+
+var b = parseInt("I am 40 years old");  // not working
+console.log(b);
+
+var c = Number("40 years");  // not working
+console.log(c);
+
+var d = parseFloat("5.67 years");
+console.log(d);
+
+
+
+
+
+
+
+
