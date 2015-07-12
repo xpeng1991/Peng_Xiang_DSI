cea19c2165b2920a973ca15a64d9eb1384236117
xpeng1991 <xpeng@fullsail.edu>
2015-07-11 21:26:53 -0400

    ask user input
    store the input

--- Peng_Xiang_Expressions_Assignment/js/Calc.js
+++ Peng_Xiang_Expressions_Assignment/js/Calc.js
@@ -11,22 +11,43 @@
 // tell the user what to do
 
 alert("This is a simple calculator that calculates the average number.\n" +
-    "The Maximum number of numbers you may calculate is 5.\n" +
-    "Enter numbers you want to calculate\n");
+    "The Maximum number of terms you may calculate is 5.\n" +
+    "Enter numbers you want to calculate\n" +
+    "Numbers can be decimal.");
 
 // set an array to store users' input
+var index = 0;
 var arr = [0];
 
 // ask the user to do the input, and store them in the array
-arr[0] = prompt("Please enter a number you want to calculate:");
-arr[1] = prompt("Please enter another number:");
-arr[2] = prompt("Please enter another number:\n" +
-    "if no more numbers to be calculated, just simply enter '0' ");
-arr[3] = prompt("Please enter another number:\n" +
-    "if no more numbers to be calculated, just simply enter '0' ");
-arr[4] = prompt("Please enter another number:\n" +
-    "if no more numbers to be calculated, just simply enter '0' ");
+arr[index] = prompt("Please enter a number you want to calculate:");
+index++;
+arr[index] = prompt("Please enter another number:");
+index++;
+arr[index] = prompt("Please enter another number:");
+index++;
+arr[index] = prompt("Please enter another number:");
+index++;
+arr[index] = prompt("Please enter another number:");
 
 console.log(arr);
 
 // calculate the average value based on users' input
+var value = (parseFloat(arr[0]) + parseFloat(arr[1]) + parseFloat(arr[2])
+    + parseFloat(arr[3]) + parseFloat(arr[4])) / 5;
+
+// show us the result
+var answer = "You entered: " + arr[0] + ", " + arr[1] + ", " + arr[2] + ", " +
+        arr[3] + ", " + arr[4] + ", and the average value is: " + value + ".";
+
+console.log(answer);
+alert(answer);
+
+
+/*
+test 1: entered: 15, 20, 25, 30, 50, and got answer: 28
+test 2: entered: 7, 15, 44, 32, 9, and got answer: 21.4
+test 3: entered: 11.3, 56.3, 24.9, 67.2, 12.9, and got answer: 34.519999999999996
+ */
+
+
