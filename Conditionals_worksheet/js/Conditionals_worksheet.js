cdd1907a9d00cd031198f4012465c18bf386e06f
xpeng1991 <xpeng@fullsail.edu>
2015-07-14 20:30:11 -0400

    Finished first problem
    Temperature converter

--- Conditionals_worksheet/js/Conditionals_worksheet.js
+++ Conditionals_worksheet/js/Conditionals_worksheet.js
@@ -7,3 +7,33 @@
 
 
 //alert("Testing to see if this works");
+
+
+//// Celsius to Fahrenheit converter
+
+// ask the user for temperature
+var number = prompt("Please enter the temperature you want to convert: ");
+console.log(number);
+
+// ask the user which one wants to convert
+var ForC = prompt("Please enter which one you want to convert to.\n" +
+    "Enter 'F' or 'C' ;");
+console.log(ForC);
+
+// store the temperature
+var temp = 0;
+
+
+// C to F
+if (ForC === 'F' || ForC === 'f')
+{
+ temp = (number - 32) * (5 / 9);
+ alert("The temperature is " + temp + " degrees Fahrenheit");
+}
+
+// F to C
+if (ForC === 'C' || ForC === 'c')
+{
+ temp = number * (9 / 5) + 32;
+ alert("The temperature is " + temp + " degrees Celsius.");
+}
