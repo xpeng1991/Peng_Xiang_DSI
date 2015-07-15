45a487310378fafff03dda1aa4ae535dbc763edd
xpeng1991 <xpeng@fullsail.edu>
2015-07-14 20:36:33 -0400

    Finished problem 2
    and fixed bug in the first problem

--- Conditionals_worksheet/js/Conditionals_worksheet.js
+++ Conditionals_worksheet/js/Conditionals_worksheet.js
@@ -9,7 +9,10 @@
 //alert("Testing to see if this works");
 
 
-//// Celsius to Fahrenheit converter
+
+
+
+// Celsius to Fahrenheit converter
 
 // ask the user for temperature
 var number = prompt("Please enter the temperature you want to convert: ");
@@ -24,16 +27,70 @@ console.log(ForC);
 var temp = 0;
 
 
-// C to F
-if (ForC === 'F' || ForC === 'f')
-{
- temp = (number - 32) * (5 / 9);
- alert("The temperature is " + temp + " degrees Fahrenheit");
-}
-
 // F to C
 if (ForC === 'C' || ForC === 'c')
 {
- temp = number * (9 / 5) + 32;
- alert("The temperature is " + temp + " degrees Celsius.");
+ temp = (number - 32) * (5 / 9);
+ alert("The temperature is " + temp + " degrees Celsius");
 }
+
+// C to F
+if (ForC === 'F' || ForC === 'f')
+{
+ temp = number * (9 / 5) + 32;
+ alert("The temperature is " + temp + " degrees Fahrenheit.");
+}
+
+
+
+
+
+// Grade Letter Calculator
+
+// ask for a number grade
+var grade = prompt("Please enter your number grade: ");
+
+// store the letter grade
+var letterGrade = "0";
+
+
+// grades under different conditions
+if (grade >= 95 && grade <= 100)
+ letterGrade = "A+";
+
+if (grade >= 90 && grade <= 94)
+ letterGrade = "A";
+
+if (grade >= 85 && grade <= 89)
+ letterGrade = "B+";
+
+if (grade >= 80 && grade <= 84)
+ letterGrade = "B";
+
+if (grade >= 76 && grade <= 79)
+ letterGrade = "C+";
+
+if (grade >= 73 && grade <= 75)
+ letterGrade = "C";
+
+if (grade >= 70 && grade <= 72)
+ letterGrade = "D";
+
+if (grade >= 0 && grade <= 69)
+ letterGrade = "F";
+
+// show the result
+alert("You have a " + grade + "%, which means you have earned a(n) " +
+    letterGrade + " in the class!");
+
+
+
+
+
+
+
+
+
+
+
+
