b0bb32754567c7c6f0374b26ccd67710e51f0981
xpeng1991 <xpeng@fullsail.edu>
2015-07-16 17:57:24 -0400

    finished gender factor
    finished gender factor

--- Peng_Xiang_Conditionals_Assignment/js/Lucky_Number_Calculator.js
+++ Peng_Xiang_Conditionals_Assignment/js/Lucky_Number_Calculator.js
@@ -31,20 +31,69 @@ do
 
 } while(age === 0 || age === "" || (age <=0 || age >= 300) || isNaN(age));
 
-console.log(age);
+console.log("Age: " + age);
 
 // ask for gender
 var gender = true;   // gender factor
-var genderInput = 0
+var genderInput = 0;
+var check = 0; // input checker
+
 do
 {
-
  genderInput = prompt("Are you a male or a female?\n" +
      "M for male and F for female.");
 
-} while (genderInput === 0 || genderInput === "" ||
-               (genderInput != 'M' || genderInput != 'm') ||
-                genderInput != 'F' || genderInput != 'f');
+ if (genderInput === 'M' || genderInput === 'm')
+ {
+  gender = true;
+  check = 1;
+ }
+
+ else if (genderInput === 'F' || genderInput === 'f')
+ {
+  gender = false;
+  check = 1;
+ }
+ else
+ {
+  genderInput = 0;
+  check = 0;
+ }
+
+} while (check === 0);
+
+console.log("Gender: " + genderInput);
+console.log(gender);
+
+// get the final gender factor number
+var genderNumber = (gender ? 2.5 : 5.2);
+console.log(genderNumber);
+
+
+// Get three random numbers from users
+// Getting first number
+var num1 = 0;
+do
+{
+ num1 = prompt("Please enter a random number between 0 to 10, can be decimal");
+} while(num1 > 10 || num1 < 0 || isNaN(num1) || num1 === "");
+console.log(num1);
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
