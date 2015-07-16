0f167a9f78a63b089d3aa5db5fde338506d47935
xpeng1991 <xpeng@fullsail.edu>
2015-07-16 18:09:59 -0400

    get three random numbers
    fixed bugs and finished

--- Peng_Xiang_Conditionals_Assignment/js/Lucky_Number_Calculator.js
+++ Peng_Xiang_Conditionals_Assignment/js/Lucky_Number_Calculator.js
@@ -18,57 +18,57 @@ Age, Gender, Three random numbers from user, another random factor.
  */
 
 
+if (0) {
+
 // Basic introduction
-alert("This is a lucky number calculator.");
-alert("Please enter your answer after questions pop up :)");
+ alert("This is a lucky number calculator.");
+ alert("Please enter your answer after questions pop up :)");
 
 // ask for age
-var age = 0;
-do
-{
+ var age = 0;
+ do
+ {
 
- age = prompt("Please enter your age here, you can use decimal if you want.");
+  age = prompt("Please enter your age here, you can use decimal if you want.");
 
-} while(age === 0 || age === "" || (age <=0 || age >= 300) || isNaN(age));
+ } while (age === 0 || age === "" || (age <= 0 || age >= 300) || isNaN(age));
 
-console.log("Age: " + age);
+ console.log("Age: " + age);
 
 // ask for gender
-var gender = true;   // gender factor
-var genderInput = 0;
-var check = 0; // input checker
+ var gender = true;   // gender factor
+ var genderInput = 0;
+ var check = 0; // input checker
 
-do
-{
- genderInput = prompt("Are you a male or a female?\n" +
-     "M for male and F for female.");
-
- if (genderInput === 'M' || genderInput === 'm')
+ do
  {
-  gender = true;
-  check = 1;
- }
+  genderInput = prompt("Are you a male or a female?\n" +
+      "M for male and F for female.");
 
- else if (genderInput === 'F' || genderInput === 'f')
- {
-  gender = false;
-  check = 1;
- }
- else
- {
-  genderInput = 0;
-  check = 0;
- }
+  if (genderInput === 'M' || genderInput === 'm') {
+   gender = true;
+   check = 1;
+  }
 
-} while (check === 0);
+  else if (genderInput === 'F' || genderInput === 'f') {
+   gender = false;
+   check = 1;
+  }
+  else {
+   genderInput = 0;
+   check = 0;
+  }
 
-console.log("Gender: " + genderInput);
-console.log(gender);
+ } while (check === 0);
+
+ console.log("Gender: " + genderInput);
+ console.log(gender);
 
 // get the final gender factor number
-var genderNumber = (gender ? 2.5 : 5.2);
-console.log(genderNumber);
+ var genderNumber = (gender ? 2.5 : 5.2);
+ console.log(genderNumber);
 
+} /// test
 
 // Get three random numbers from users
 // Getting first number
@@ -79,10 +79,21 @@ do
 } while(num1 > 10 || num1 < 0 || isNaN(num1) || num1 === "");
 console.log(num1);
 
-// 
+// Getting second number
+var num2 = 0;
+do
+{
+ num2 = prompt("Please enter a random number between 10 to 100, can be decimal");
+} while(num2 > 100 || num2 < 10 || isNaN(num2) || num2 === "" || num2 === 0);
+console.log(num2);
 
-
-
+// Getting third number
+var num3 = 0;
+do
+{
+ num3 = prompt("Please enter a random number between 100 to 1000, can be decimal");
+} while(num3 > 1000 || num3 < 100 || isNaN(num3) || num3 === "" || num3 === 0);
+console.log(num3);
 
 
 
