6c77d93f7caec7779e0be223625ccef90f05b044
xpeng1991 <xpeng@fullsail.edu>
2015-07-18 21:32:02 -0400

    Started worksheet
    worksheet

---
+++ Loops_worksheet/js/Loops_worksheet.js
@@ -0,0 +1,63 @@
+/*
+ Xiang Peng
+ Section 00
+ 7-18-15
+ Loops_worksheet
+ */
+
+
+//alert("Testing to see if this works");
+
+// While Loop
+// These are simple examples of loops
+
+// user enter a random through 1 to 10
+// then there will be popup window that shows the number user entered
+// this popup window will show X times depends on users input
+var num = 0;
+
+do
+{
+
+ num = prompt("Please enter a number between 1 - 10");
+
+} while ((((num < 1 || num > 10 ) || num === 0 )|| num === "" )|| isNaN(num));
+
+var x = 0;
+
+while (x < num)
+
+{
+ x++;
+ alert("you entered number: " + num + "\nThis is [" + x + "] time this shows up.");
+}
+
+
+
+// Do While Loop
+
+var fruit = "x";
+fruit = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// For Loop
+
+
