596d4cd4be76f8290fcef5e63fdbaba04fb55cd7
xpeng1991 <xpeng@fullsail.edu>
2015-07-18 21:50:40 -0400

    update
    update

--- Loops_worksheet/js/Loops_worksheet.js
+++ Loops_worksheet/js/Loops_worksheet.js
@@ -14,31 +14,49 @@
 // user enter a random through 1 to 10
 // then there will be popup window that shows the number user entered
 // this popup window will show X times depends on users input
-var num = 0;
+if (0) {
+ var num = 0;
 
-do
-{
+ do
+ {
 
- num = prompt("Please enter a number between 1 - 10");
+  num = prompt("Please enter a number between 1 - 10");
 
-} while ((((num < 1 || num > 10 ) || num === 0 )|| num === "" )|| isNaN(num));
+ } while ((((num < 1 || num > 10 ) || num === 0 ) || num === "" ) || isNaN(num));
 
-var x = 0;
+ var x = 0;
 
-while (x < num)
-
-{
- x++;
- alert("you entered number: " + num + "\nThis is [" + x + "] time this shows up.");
+ while (x < num) {
+  x++;
+  alert("you entered number: " + num + "\nThis is [" + x + "] time this shows up.");
+ }
 }
-
 
 
 // Do While Loop
 
-var fruit = "x";
-fruit = 
+var fruit = ["x"];
+var numFruit = 0;
+do
+{
+ numFruit = prompt("How many fruit do you want? (1 - 5)");
+} while((((numFruit < 1 || numFruit > 5 )|| numFruit === 0 )|| numFruit === "" )|| isNaN(numFruit));
+console.log(numFruit);
 
+var times = 0;
+do
+{
+ do
+ {
+  fruit[times] = prompt("Which one do you want to eat? (Simple enter the first letter)\n" +
+      "[O]Orange\n" +
+      "[A]Apple\n" +
+      "[G]Grape\n" +
+      "[L]Lemon");
+ } while (((fruit.toLowerCase() != "o" || fruit.toLowerCase() != "a" )|| fruit.toLowerCase() != "g" )||
+     fruit.toLowerCase() != "l");
+ times ++;
+} while (times <= numFruit);
 
 
 
