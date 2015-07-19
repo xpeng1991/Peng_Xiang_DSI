6c3539c66551aefddd6fda8437c8e40e9ee7c0b4
xpeng1991 <xpeng@fullsail.edu>
2015-07-18 22:54:34 -0400

    finished
    finished

--- Loops_worksheet/js/Loops_worksheet.js
+++ Loops_worksheet/js/Loops_worksheet.js
@@ -16,9 +16,12 @@
 // user enter a random through 1 to 10
 // then there will be popup window that shows the number user entered
 // this popup window will show X times depends on users input
-if (0) {
+if (1)
+{
+ // basic var
  var num = 0;
 
+ // get input
  do
  {
 
@@ -26,6 +29,7 @@ if (0) {
 
  } while ((((num < 1 || num > 10 ) || num === 0 ) || num === "" ) || isNaN(num));
 
+ // show the result
  var x = 0;
 
  while (x < num) {
@@ -35,53 +39,67 @@ if (0) {
 }
 
 
-// Do While Loop
-if (0) {
- var fruit = 0;
- var numFruit = 0;
 
+
+// Do While Loop
+if (1) {
+ var fruit = 0;
+ var count = 0;
+
+// until the user enters 'y', this will keeps poping up
  do
  {
-  fruit = prompt("Do you want to eat fruit? (Y/N)");
- } while (((fruit != 'y' || fruit != 'n' ) ||
- fruit != 'Y' ) || fruit != 'N');
+  // getting input from user
+  do
+  {
+   fruit = prompt("Do you want to eat fruit? (Y/N)");
+  } while (fruit.toLowerCase() != 'y' && fruit.toLowerCase() != 'n');
 
+  // if the user entered yes, this loops is over and show the result
+  if (fruit == 'y') {
+   alert("Good, you are healthy!");
+  }
 
+  // if the user entered now, this will keeps popping up several times
+  // then go back to the beginning and ask again
+  if (fruit == 'n')
+  {
+   count = 0;
+   do
+   {
+    alert("Eat more fruit!");
+    count++;
+   } while (count < 9)
+   fruit = "n";
+  }
+ } while (fruit.toLowerCase() != 'y');
 }
 
 
 
 
-
-
-
-
-
-
-
-
-if(0)
-{
+ if (1) {
 // For Loop
 
 // basic vars
- var num2 = 0;
- var star = [0];
+  var num2 = 0;
+  var star = [0];
 
 // ask user how many stars they want, input valid
- do
- {
-  num2 = prompt("How many stars do you want? (1 - 10)");
- } while (((num2 > 10 || num2 < 1 ) || num2 === "" ) || isNaN(num2));
+  do
+  {
+   num2 = prompt("How many stars do you want? (1 - 10)");
+  } while (((num2 > 10 || num2 < 1 ) || num2 === "" ) || isNaN(num2));
 
 // put stars into array
- for (var i = 0; i < num2; i++) {
-  star[i] = "* ";
- }
+  for (var i = 0; i < num2; i++) {
+   star[i] = "* ";
+  }
 
 // show the answer
- alert("You have " + num2 + " stars!\n" + star);
-}
+  alert("You have " + num2 + " stars!\n" + star);
+ }
+
 
 
 
