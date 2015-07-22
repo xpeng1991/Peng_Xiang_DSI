35a16ff902dd8b9724f3ffa3caeacc1f534484bc
xpeng1991 <xpeng@fullsail.edu>
2015-07-21 22:18:04 -0400

    user input
    getting all the input and loop thru

--- ArrayLoop_worksheet/js/ArrayLoop_worksheet.js
+++ ArrayLoop_worksheet/js/ArrayLoop_worksheet.js
@@ -19,8 +19,13 @@ do
 {
  do
  {
-  arr[i] = prompt("Please enter a number:");
- } while (arr[i] == "" || isNaN(arr[i]));
+  arr[i] = prompt("Please enter a number from 1 - 100");
+ } while (((arr[i] == "" || isNaN(arr[i]) )|| arr[i] > 100 )|| arr[i] < 1);
  i++;
-} while (i < 9);
+} while (i < 4);
 
+// show user the record
+console.log("Array: " + arr);
+alert("You entered: " + arr);
+
+// show other results
