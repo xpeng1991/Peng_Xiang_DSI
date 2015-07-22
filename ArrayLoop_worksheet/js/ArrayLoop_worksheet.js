0563d6bdadc49c0730a40e066d40be96a26252f4
xpeng1991 <xpeng@fullsail.edu>
2015-07-21 22:14:34 -0400

    number recorder
    getting user input

--- ArrayLoop_worksheet/js/ArrayLoop_worksheet.js
+++ ArrayLoop_worksheet/js/ArrayLoop_worksheet.js
@@ -10,3 +10,17 @@
 
 
 
+//ArrayLoop
+var arr = [0];
+var i = 0;
+
+// ask for user's input
+do
+{
+ do
+ {
+  arr[i] = prompt("Please enter a number:");
+ } while (arr[i] == "" || isNaN(arr[i]));
+ i++;
+} while (i < 9);
+
