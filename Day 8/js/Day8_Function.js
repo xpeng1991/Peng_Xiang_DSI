a8eaa41fb0f0d8b86185906a5a6c6cde80095a3b
xpeng1991 <xpeng@fullsail.edu>
2015-07-23 17:55:48 -0400

    function
    function

--- Day 8/js/Day8_Function.js
+++ Day 8/js/Day8_Function.js
@@ -9,9 +9,37 @@
 //alert("Testing to see if this works");
 
 
+// Basic function structure
+// function functionName()
+
+// create a function that will console.log something
+
+function printHello()
+{
+ // this code will run when function runs
+ console.log("This function is working!")
+}
+
+// create a function call that will start printHello
+printHello();
 
 
 
+// create a function that console.log a different string
+function printMore()
+{
+ console.log("This is different text.");
+}
+
+// function call
+printMore();
+
+
+// function calculates the area of a rectangle
+function calcArea()
+{
+ // 
+}
 
 
 
