3ebcadc00aa9776eb51a92a8d2ebf2b5a0d2380e
xpeng1991 <xpeng@fullsail.edu>
2015-07-23 20:18:34 -0400

    getting started
    getting started

---
+++ Functions_worksheet/js/Functions_worksheet.js
@@ -0,0 +1,22 @@
+/*
+ Xiang Peng
+ Section 00
+ 7-23-15
+ Functions_worksheet
+ */
+
+
+//alert("Testing to see if this works");
+
+
+
+
+// Circumference
+// create function
+function cirCalc(rad)
+{
+ return circ = rad * 3.14 * 2;
+}
+
+// get a simple number and call the function
+
