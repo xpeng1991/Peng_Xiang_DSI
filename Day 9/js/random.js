20f6c235dc383e344bf00696cd07af7a54b3f743
xpeng1991 <xpeng@fullsail.edu>
2015-07-25 17:44:17 -0400

    min value
    min value

--- Day 9/js/random.js
+++ Day 9/js/random.js
@@ -10,8 +10,25 @@
 
 
 
+// a function takes max and min and then return a random number between them
 
 
+//Create a variable for the min value
+var min = prompt("We are going to find a random number between 2 values.\nWhat is the minimum value?");
+//Validate for min
+while(isNaN(min) || min ==="")
+{
+ if(isNaN(min))
+ {
+  min = prompt("Please only enter a number value.\nWhat is the minimum value?");
+ }
+ else
+ {
+  min = prompt("Please do not leave blank.\nWhat is the minimum value?");
+ }
+}
+//Number cast or parseInt
+min = Number(min);
 
 
 
