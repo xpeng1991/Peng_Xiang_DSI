7b5ffde1d2d2c296c68ed39e8e3d26037be6e0be
xpeng1991 <xpeng@fullsail.edu>
2015-07-25 20:48:56 -0400

    anon function
    finished

--- Peng_Xiang_Function_Assignment/js/Peng_Xiang_Function_Assignment.js
+++ Peng_Xiang_Function_Assignment/js/Peng_Xiang_Function_Assignment.js
@@ -22,31 +22,50 @@ var volume = 0;
 // width
 do
 {
- width = prompt("Please enter the width of a cube that you want to calculate (no zero):")
+ width = prompt("Please enter the width of a cube that you want to calculate (no zero):");
+ width = Number(width);
 } while(width == 0 || width == "" || isNaN(width));
+console.log("width: " + width);
 
 // length
 do
 {
- length = prompt("Please enter the length of a cube that you want to calculate (no zero):")
+ length = prompt("Please enter the length of a cube that you want to calculate (no zero):");
+ length = Number(length);
 } while(length == 0 || length == "" || isNaN(length));
+console.log("length: " + length);
 
 // height
 do
 {
- height = prompt("Please enter the width of a cube that you want to calculate (no zero):")
+ height = prompt("Please enter the width of a cube that you want to calculate (no zero):");
+ height = Number(height);
 } while(height == 0 || height == "" || isNaN(height));
+console.log("height: " + height);
+
 
 // call the function and catch the return value
-volume = cube;
-
-// anonymous function
-
+volume = cube(width, length, height);
+console.log("volume: " + volume);
 
 
 
+/////////////////////////////////////////////////////
+// anonymous function that shows the user result
+var answer = function(v)
+{
+ var result = "the volume of this cube is: " + v + " cubic unit.";
+ return result;
+};
+
+// call the function to show the result
+var showUser = answer(volume);
+console.log(showUser);
+alert(showUser);
 
 
+
+/////////////////////////////////////////////////
 // A normal function that calculates the volume
 function cube(w, l, h)
 {
