3d28cce37d8aad2b473fbcc45c344cc6bc5a0760
xpeng1991 <xpeng@fullsail.edu>
2015-07-28 21:30:51 -0400

    update
    update

--- Day 10 Review/Review/Day 3/js/day3_prompts.js
+++ Day 10 Review/Review/Day 3/js/day3_prompts.js
@@ -11,8 +11,10 @@
 
 // ask user for information
 // input
-var userInput = prompt("Enter your year of birth:");
-
+do
+{
+    var userInput = prompt("Enter your year of birth:");
+} while (((userInput == "" || isNaN(userInput) )|| userInput < 0 )|| userInput > 2015);
 console.log(userInput);
 
 var age = 2015 - userInput;
@@ -22,16 +24,23 @@ console.log(age);
 // calculate the area of a rectangle
 
 // prompt the user for width
-var width = prompt("Let's calculate the area of a rectangle.\n" +
-    "Please enter a width");
-
+do
+{
+    var width = prompt("Let's calculate the area of a rectangle.\n" +
+        "Please enter a width");
+} while (width == "" || isNaN(width));
 // prompt the user for height
-var height = prompt("Please enter a height");
+do
+{
+    var height = prompt("Please enter a height");
+} while (height == "" || isNaN(height));
 
 // calculate the area using the user input
 var area = width * height;
 console.log(area);
 
+
 // create a variable that holds the whole result
 var result = "The area of your rectangle is " + area;
 console.log(result);
+alert("The area of your rectangle is " + area);
